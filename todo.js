5ea4c3d0c561666601bc0e76679c4d391c619e03
vonot16 <rafaelfeliperm@gmail.com>
2022-05-03 17:16:57 -0300

    started modal

--- todo.js
+++ todo.js
@@ -1,4 +1,32 @@
-class todo extends HTMLElement{
+const addButton = document.querySelector("#add-item")
+const todoItems = []
+const todoMain = document.querySelector("#list")
+
+addButton.addEventListener("click",() => {
+    let modal = document.createElement("modal-form")
+    document.body.appendChild(modal)
+})
+
+class todoItem extends HTMLElement{
+    constructor(){
+        super()
+        this.create()
+    }
+    create(){
+        const shadow = this.attachShadow({mode:"open"})
+        let item = document.createElement("div")
+        shadow.appendChild(item)
+    }
+
+    style(element){
+        element.classList.add = "teste"
+        element.style.height = "100px:"
+        element.style.width = "100%"
+        element.style.backgroundColor = "red"
+    }
+}
+
+class modalForm extends HTMLElement{
     constructor(){
         super()
         this.create()
@@ -6,10 +34,14 @@ class todo extends HTMLElement{
 
     create(){
         const shadow = this.attachShadow({mode:"open"})
-
-        let div = document.createElement("div")
-        shadow.appendChild(div)
+        let form = document.createElement("div")
+        form.style.width = "500px";
+        form.style.height = "500px";
+        let confirmButton = document.createElement("buttom")
+        confirmButton.innerText = "Save"
+        form.appendChild(confirmButton)
+        shadow.appendChild(form)
     }
 }
-
-customElements.define('todo-element',todo)
+customElements.define('modal-form',modalForm)
+customElements.define('todo-item',todoItem)
